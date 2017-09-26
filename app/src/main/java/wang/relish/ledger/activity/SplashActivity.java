8893efbfc6fe7e8b90faa8d1e7fedca66ebd4ccb
王鑫 <wangxin@souche.com>
2017-09-26 20:19:02 +0800

    fuck: 突然想做积累了

---
+++ app/src/main/java/wang/relish/ledger/activity/SplashActivity.java
@@ -0,0 +1,22 @@
+package wang.relish.ledger.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import wang.relish.ledger.R;
+
+/**
+ * @author Relish Wang
+ * @since 2017/09/26
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
