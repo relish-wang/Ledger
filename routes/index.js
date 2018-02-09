ab1b56c6214becfd1309ccbbbd3a0c5afcc0c9fb
王鑫 <wangxin@souche.com>
2018-02-09 14:41:39 +0800

    feat: add routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '主页' });
+});
+router.get('/reg', function(req, res) {
+  res.render('reg', { title: '注册' });
+});
+router.post('/reg', function(req, res) {
+
+});
+router.get('/login', function(req, res) {
+  res.render('login', { title: '登录' });
+});
+router.post('/login', function(req, res) {
+
+});
+router.get('/post', function(req, res) {
+  res.render('post', { title: '发表' });
+});
+router.post('/post', function(req, res) {
+
+});
+router.get('/logout', function(req, res) {
+
 });
 
 module.exports = router;
